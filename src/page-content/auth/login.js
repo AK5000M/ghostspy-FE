9eb9ecaebc631a8cb704e7295f4ce274291ae8f2
Crystal_P <dgon8599@gmail.com>
2024-09-13 18:39:21 -0700

    change ip api

--- src/page-content/auth/login.js
+++ src/page-content/auth/login.js
@@ -143,8 +143,10 @@ export const LoginContent = () => {
   // Helper function to fetch public IP address
   const fetchPublicIP = async () => {
     try {
-      const response = await fetch("https://api.ipify.org?format=json");
+      // const response = await fetch("https://api.ipify.org?format=json");
+      const response = await fetch("https://ipapi.co/json/");
       const data = await response.json();
+      console.log({ data });
       return data.ip;
     } catch (error) {
       console.error("Error fetching IP:", error);
--- src/page-content/auth/register.js
+++ src/page-content/auth/register.js
@@ -126,7 +126,8 @@ export const RegisterContent = () => {
   // Helper function to fetch public IP address
   const fetchPublicIP = async () => {
     try {
-      const response = await fetch("https://api.ipify.org?format=json");
+      // const response = await fetch("https://api.ipify.org?format=json");
+      const response = await fetch("https://ipapi.co/json/");
       const data = await response.json();
       return data.ip;
     } catch (error) {
