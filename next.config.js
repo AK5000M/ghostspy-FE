55e5f24139f970931f64b657c910c9b2331fcfed
Crystal_P <dgon8599@gmail.com>
2024-09-20 10:33:43 -0700

    apply helmet for xss security

--- next.config.js
+++ next.config.js
@@ -11,4 +11,29 @@ module.exports = {
     // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
+
+  // webpack: (config, { isServer }) => {
+  //   if (!isServer) {
+  //     config.output.filename = "static/chunks/[name].js";
+  //   }
+  //   return config;
+  // },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          { key: "X-Content-Type-Options", value: "nosniff" },
+          { key: "X-Frame-Options", value: "DENY" },
+          { key: "Referrer-Policy", value: "no-referrer" },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=63072000; includeSubDomains; preload",
+          },
+          { key: "X-XSS-Protection", value: "1; mode=block" },
+        ],
+      },
+    ];
+  },
 };
