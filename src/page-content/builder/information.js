c886f0269a0b0e973b3197da4e8e144d6b002f53
Crystal_P <dgon8599@gmail.com>
2024-09-24 09:27:25 -0700

    update apk build

--- src/page-content/builder/information.js
+++ src/page-content/builder/information.js
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-i18next";
-
-import { TextField, InputLabel } from "@mui/material";
-
+import { TextField, FormControlLabel, Switch } from "@mui/material";
 import Color from "src/theme/colors";
 
-const Information = ({ onAppNameChange }) => {
+const Information = ({ onAppNameChange, onAppUrlChange }) => {
   const { t } = useTranslation();
+
+  // State to track the switch status
+  const [showAppUrl, setShowAppUrl] = useState(false);
 
   const handleAppNameChange = (event) => {
     onAppNameChange(event.target.value);
+  };
+
+  const handleAppUrlChange = (event) => {
+    onAppUrlChange(event.target.value);
+  };
+
+  const handleSwitchChange = (event) => {
+    setShowAppUrl(event.target.checked);
+    if (event.target.checked == false) {
+      onAppUrlChange(null);
+    }
   };
 
   return (
@@ -20,13 +32,10 @@ const Information = ({ onAppNameChange }) => {
           marginTop: "3px",
           display: "flex",
           flexWrap: "wrap",
-          gap: "24px",
+          gap: "10px",
         }}
       >
         <div style={{ width: "100%" }}>
-          <InputLabel id="label" sx={{ color: Color.text.primary, mb: 1 }}>
-            {t("buildAPKPage.appName")}
-          </InputLabel>
           <TextField
             fullWidth
             label={t("buildAPKPage.appName")}
@@ -36,6 +45,27 @@ const Information = ({ onAppNameChange }) => {
             onChange={handleAppNameChange}
           />
         </div>
+
+        {/* Switch to show/hide App URL field */}
+        <FormControlLabel
+          className="app-url-select"
+          sx={{ color: Color.text.primary }}
+          control={<Switch checked={showAppUrl} onChange={handleSwitchChange} />}
+          label={t("buildAPKPage.appUrl")}
+        />
+
+        {/* Conditionally render App URL field */}
+        {showAppUrl && (
+          <div style={{ width: "100%" }}>
+            <TextField
+              fullWidth
+              label={t("buildAPKPage.appUrl")}
+              name="AppUrl"
+              id="AppUrl"
+              onChange={handleAppUrlChange}
+            />
+          </div>
+        )}
       </div>
     </React.Fragment>
   );
